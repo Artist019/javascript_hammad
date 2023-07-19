d06dfc899a5d7c95efbfdca06775dee3a172099f
Artist019 <139927898+Artist019@users.noreply.github.com>
2023-07-19 17:06:53 +0000

    datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let name = "hamamd"
+let id = 1
+let status = false
+let address = null
+let gmail 
+
+
+// console.log(typeof name);
+// console.log(typeof id);
+// console.log(typeof status);
+// console.log(typeof address);
+// console.log(typeof gmail);
+
+
+let convertName = Number(name)
+// console.log(typeof convertName);
+// console.log(convertName);
+
+let convertStatus = Number(status)
+// console.log(typeof convertStatus);
+// console.log(convertStatus);
+
+let convertId = String(id)
+// console.log(typeof convertId);
+// console.log(convertId);
+
+let convertAddress = Boolean(address)
+// console.log(typeof(convertAddress));
+// console.log(convertAddress);
+
+let convertGmail = Boolean(gmail)
+// console.log(typeof(convertGmail));
+// console.log(convertGmail);
