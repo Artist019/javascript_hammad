569cf7362c28557008fb7815e19fdbefe471100e
Artist019 <139927898+Artist019@users.noreply.github.com>
2023-07-19 17:41:29 +0000

    config

---
+++ 01_basic/operation.js
@@ -0,0 +1,33 @@
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let value = -(-4)
+let negValue = -value
+// console.log(negValue);
+
+let num1
+let num2 = "hello" 
+let num3 = " hammad"
+num1 = num2 + num3
+// console.log(num1);
+
+// console.log("1" + 2 + 3);
+// console.log(1 + "2" + 3);
+// console.log(1 + 2 + "3");
+
+
+let count = 10
+count--
+// console.log(count);
+
+let a = 5
+let c = a++  // first it stores value in c and then increment the value
+let b = ++a  // first increment happens then it store value in b
+// console.table([a,b]);
+
+console.log( 3 + 4 * 5 / 2);
+console.log( (3 + 4) * 5 / 2);  // precedence
