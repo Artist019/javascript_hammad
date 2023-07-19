9c604be70e76ffea1f65ad0c2962c40e79b32718
Artist019 <139927898+Artist019@users.noreply.github.com>
2023-07-19 13:31:33 +0000

    container

---
+++ 01_basic/var.js
@@ -0,0 +1,10 @@
+const custId = 123
+let custName = "hammad"
+var custAddress = "people colony"
+custGmail = "hammad@gmail.com"
+
+custName = "awais"
+custAddress = "madni colony"
+custGmail = "ha@gmail.com"
+
+console.table([custId,custName,custAddress,custGmail]);
